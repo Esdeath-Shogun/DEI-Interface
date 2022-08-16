5d60630e249b717859b571beab12d427e5346478
Daniel Gonçalves <61202556+Esdeath-GK@users.noreply.github.com>
2022-08-16 21:07:53 +0100

    Interface update

--- dashboard/js/main.js
+++ dashboard/js/main.js
@@ -5,7 +5,6 @@ const timerText = document.getElementById("timer");
 // Method Declaration
 function updateTime(){
     timerText.innerText = new Date().toLocaleTimeString("pt-PT", {timeZone: "Europe/Lisbon",}).replace(", ", " - ");
-    setInterval(updateTime.bind(this), 1000);
 }
 
 
@@ -32,3 +31,4 @@ function updateTime(){
 
 // Script Execution
 updateTime();
+setInterval(updateTime.bind(this), 1000);
